e8d10fa40b427d4d581ec22a7df217a70af64977
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-05 19:54:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# Made with python3
+# (C) @FayasNoushad
+# Copyright permission under MIT License
+# All rights reserved by FayasNoushad
+# License -> https://github.com/FayasNoushad/Channel-Auto-Post-Bot/blob/main/LICENSE
+
 import os
 from pyrogram import Client
 
