e034a80814630c15e6bbfaa138fc69cd5d918ad0
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-05 12:09:21 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+from pyrogram import Client
+
+FayasNoushad = Client(
+    "Channel Auto Post Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
