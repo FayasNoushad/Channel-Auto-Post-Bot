13507bcfdf4951e836b5646c38d9498361e4a4f5
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-05 12:11:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,12 @@ FayasNoushad = Client(
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"]
 )
+
+@FayasNoushad.on_message(filters.channel & (filters.media | filters.text))
+async def autopost(bot, update):
+    if not update.chat.id in FROM_CHANNELS:
+        return
+    try:
+        await update.copy(chat_id=TO_CHAT)
+    except Exception as error:
+        print(error)
