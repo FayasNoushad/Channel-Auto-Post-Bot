ef8ffc73471ec536326b8f1dd5b93b876d020a15
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-05 12:10:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 from pyrogram import Client
 
+FROM_CHANNELS = int(os.environ["FROM_CHANNELS"])
+TO_CHAT = int(os.environ["TO_CHAT"])
+
 FayasNoushad = Client(
     "Channel Auto Post Bot",
     bot_token = os.environ["BOT_TOKEN"],
