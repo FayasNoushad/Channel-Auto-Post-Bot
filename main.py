0b35154cb80a7736989c68183c39c6fa111da566
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-11 11:11:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 
 import os
 from pyrogram import Client
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 FROM_CHANNELS = set(int(x) for x in os.environ.get("FROM_CHANNELS", "").split())
 TO_CHAT = int(os.environ["TO_CHAT"])
@@ -22,12 +23,21 @@ Hello {}, I am a channel auto post telegram bot.
 
 Made by @FayasNoushad
 """
+START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Channel', url='https://telegram.me/FayasNoushad'),
+        InlineKeyboardButton('Feedback', url='https://telegram.me/TheFayas')
+        ],[
+        InlineKeyboardButton('Source Code', url='https://github.com/FayasNoushad/Channel-Auto-Post-Bot')
+        ]]
+    )
 
 @FayasNoushad.on_message(filters.private & filters.command(["start"]))
 async def start(bot, update):
     await update.reply_text(
         text=START_TEXT.format(update.from_user.mention),
-        disable_web_page_preview=True
+        disable_web_page_preview=True,
+        reply_markup=START_BUTTONS
     )
 
 @FayasNoushad.on_message(filters.channel & (filters.media | filters.text))
