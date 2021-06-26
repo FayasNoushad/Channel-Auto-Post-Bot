b1c7278bee35dfb35079abd27fbf7fa82df9bfd3
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-26 20:53:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ FILTER_FILE = bool(os.environ.get("FILTER_FILE", True))
 FILTER_PHOTO = bool(os.environ.get("FILTER_PHOTO", True))
 FILTER_STICKER = bool(os.environ.get("FILTER_STICKER", True))
 FILTER_VIDEO = bool(os.environ.get("FILTER_VIDEO", True))
-FILTER_ANIMATION = bool(os.environ.get("", True))
+FILTER_ANIMATION = bool(os.environ.get("FILTER_ANIMATION", True))
 FILTER_VOICE = bool(os.environ.get("", True))
 FILTER_VIDEO_NOTE = bool(os.environ.get("", True))
 FILTER_CONTACT = bool(os.environ.get("", True))
@@ -62,6 +62,7 @@ async def start(bot, update):
         filters.photo if FILTER_PHOTO else None |
         filters.sticker if FILTER_STICKER else None |
         filters.video if FILTER_VIDEO else None |
+        filters.animation if FILTER_ANIMATION else None |
         filters.text
     )
 )
