9e891750813c1bf0a00d9b34733ec06692dae216
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-26 20:49:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ TO_CHAT = int(os.environ["TO_CHAT"])
 # filters for auto post
 FILTER_AUDIO = bool(os.environ.get("FILTER_AUDIO", True))
 FILTER_FILE = bool(os.environ.get("FILTER_FILE", True))
-FILTER_PHOTO = bool(os.environ.get("", True))
+FILTER_PHOTO = bool(os.environ.get("FILTER_PHOTO", True))
 FILTER_STICKER = bool(os.environ.get("", True))
 FILTER_VIDEO = bool(os.environ.get("", True))
 FILTER_ANIMATION = bool(os.environ.get("", True))
@@ -59,6 +59,7 @@ async def start(bot, update):
     filters.channel & (
         filters.audio if FILTER_AUDIO else None |
         filters.document if FILTER_FILE else None |
+        filters.photo if FILTER_PHOTO else None |
         filters.text
     )
 )
